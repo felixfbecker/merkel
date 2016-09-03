a81efd5134d8e47e4384fd6f8ec3afd8ecbd9797
Erik Seliger <eseliger@uni-bremen.de>
2016-09-03 20:39:15 +0200

    move adapter creation to adapter

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,6 +1,8 @@
 
 import {Task, Migration, TaskType} from './migration';
+import {PostgresAdapter} from './adapters/postgres';
 import {Commit} from './git';
+import {parse} from 'url';
 
 export interface TableRow {
     id: number;
@@ -17,6 +19,15 @@ export abstract class DbAdapter {
     public abstract getLastMigrationTask(): Promise<Task>;
     public abstract logMigrationTask(task: Task): Promise<void>;
     public abstract checkIfTaskCanExecute(task: Task): Promise<void>;
+
+    static getFromUrl(url: string): DbAdapter {
+        const dialect = parse(url).protocol;
+        switch (dialect) {
+            case 'postgres:': return new PostgresAdapter(url, require(process.cwd() + '/node_modules/pg'));
+            case null: throw new Error('Invalid connection URL ' + url);
+            default: throw new Error('Unssuported dialect ' + dialect);
+        }
+    }
 
     protected rowToTask(row: TableRow): Task {
         const task = new Task({
